afab3bd31107c335109a15b0e32544b006932ea7
Rajeev nandi <Rajeev nandi@RAJEEV>
2025-11-26 15:37:40 +0530

    vfsv

---
+++ svssd/src/test/java/crm/contact/Test/contactTest.java
@@ -0,0 +1,26 @@
+package crm.contact.Test;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+
+	@Test
+	public void createcontactTest()
+	{
+		System.out.println("contact created");
+	}
+	@Test
+	public void contactmodified()
+	{
+		System.out.println("contact is modified ");
+		System.out.println("hi");
+		System.out.println("hello");
+	}
+	@Test
+	public void contactmified()
+	{
+		System.out.println("contact is modified ");
+		System.out.println("hi");
+	}
+	
+}
